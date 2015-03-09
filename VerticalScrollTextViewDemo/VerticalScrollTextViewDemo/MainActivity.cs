2ea2ee4a47fe77d65138fd65202e2debf883de01
1023451515 <1023451515@57f5b8fa-0d4c-4d28-a92d-12a292a3be04>
2015-03-09 02:59:57 +0000

    垂直滚动(跑马灯)TextView

---
+++ VerticalScrollTextViewDemo/VerticalScrollTextViewDemo/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace VerticalScrollTextViewDemo
+{
+    [Activity(Label = "VerticalScrollTextViewDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            var txtMsg = FindViewById<VerticalScrollTextView>(Resource.Id.txtMsg);
+            var lst = new List<Sentence>();
+            for (var i = 0; i < 30; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    var sen = new Sentence(i, i + "、金球奖三甲揭晓 C罗梅西哈维入围 ");
+                    lst.Add(sen);
+                }
+                else
+                {
+                    var sen = new Sentence(i, i + "、公牛欲用三大主力换魔兽？？？？");
+                    lst.Add(sen);
+                }
+            }
+            //给View传递数据
+            txtMsg.SetList(lst);
+            //更新View
+            txtMsg.UpdateUi();
+        }
+    }
+}
+
